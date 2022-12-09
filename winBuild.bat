acdf8af7d5140af4085044ff3a6fc0fc12f78cfa
Gohard <chlebojad003@gmail.com>
2022-12-09 21:51:46 +0100

    build fix

--- winBuild.bat
+++ winBuild.bat
@@ -1,1 +1,12 @@
+@echo off
+REM ----------------------------------------
+REM BUILD BATCH FOR WINDOWS
+REM ----------------------------------------
+
+REM ----------------------------------------
+REM BUILD
+REM ----------------------------------------
+if not exist "build" mkdir "build"
+
+@echo on
 gcc -Wall *.c src/*.c -o build/build.exe
