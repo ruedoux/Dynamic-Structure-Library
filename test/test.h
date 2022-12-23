18b5e709c0e4919b9de0f830972f2f5079c2bc05
Gohard <chlebojad003@gmail.com>
2022-12-23 19:26:02 +0100

    CHANGE: Documentation for tests

--- test/test.h
+++ test/test.h
@@ -1,3 +1,8 @@
+/*
+ * Unit Tests for the dynamic array implementations.
+ * Mainly checking for any possible memory leaks.
+*/
+
 #pragma once
 #ifndef ARRTEST_C
 #define ARRTEST_C
@@ -30,13 +35,19 @@
 // FUNCTIONS
 // ---------------------------------------
 
+// Perform all unit tests
 void do_mem_tests();
 
+// Test a given function in a loop
 void test_function(void (*test_function)(size_t), char* testName, size_t repeats);
 
+// Generates CharArray of given size and destroys it
 void test_CA_mem(size_t size);
 
+// Generates DynamicArray of given size and destroys it
 void test_DA_mem(size_t size);
 
+// Generates List of given size and destroys it
 void test_list_mem(size_t size);
+
 #endif
